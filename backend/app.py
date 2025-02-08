193f8cfa2bb00c5f147d67df221a4bfc456d9691
AlyBozz <asolim@uwo.ca>
2025-02-08 05:42:05 -0500

    working registration and authentication

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,21 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from login import login_bp, db, login_manager
+import os
 
 app = Flask(__name__)
-CORS(app)  # Allow frontend to communicate with backend
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SECRET_KEY"] = "WOAH, THIS IS SECRET! AND TOOOTALLY RANDOM! sinjab sucks "
+CORS(app)
+
+# Initialize extensions with the app
+db.init_app(app)
+login_manager.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+app.register_blueprint(login_bp)
 
 @app.route("/")
 def home():
